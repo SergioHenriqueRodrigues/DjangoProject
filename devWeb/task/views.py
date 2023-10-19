9183c9b810e4a69fd6fa257f20e4ccbbb4c42d56
Sérgio Henrique <sergohenrique663@gmail.com>
2023-10-19 15:19:01 -0300

    SignUp Salvamento

--- devWeb/task/views.py
+++ devWeb/task/views.py
@@ -8,4 +8,13 @@ def home(request):
 
 #SignUp
 def signup(request):
-  return render(request, 'signup.html')
+  if request.method == 'GET':
+    return render(request, 'signup.html')
+  else:
+    if request.POST['password1'] == request.POST['password2']
+    try:
+      user = User.objects.create_user(username)
+
+#SignIn
+def signin(request):
+  return render(request, 'signin.html')
