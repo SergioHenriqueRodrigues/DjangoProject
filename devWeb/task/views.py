c6f3ea890583210dc17584dc377f9ad84660f133
Sérgio Henrique <sergohenrique663@gmail.com>
2023-10-20 11:04:32 -0300

    salvando novamente

--- devWeb/task/views.py
+++ devWeb/task/views.py
@@ -33,4 +33,19 @@ def signup(request):
 
 #SignIn
 def signin(request):
-  return render(request, 'signin.html')
+  if request.method == 'GET':
+    return render(request,'signin.html', {
+      'form': AuthenticationForm
+    })
+  else:
+    user = authenticate(
+      request, username=request.POST['username'], password=request.POST['password']
+    )
+    if user is None:
+      return render(request, 'signin.html', {
+        'form' : AuthenticationForm,
+        'error': 'Usuário ou senha está incorreto'
+      })
+    else:
+      login(request, user)
+      return redirect('tasks')
