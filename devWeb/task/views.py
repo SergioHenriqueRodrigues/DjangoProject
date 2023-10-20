e6df430273da004f3d21549e54901bc93ae9d96e
Sérgio Henrique <sergohenrique663@gmail.com>
2023-10-20 10:48:03 -0300

    Signup funcionando parcialmente finalmente

--- devWeb/task/views.py
+++ devWeb/task/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
+from django.shortcuts import render, redirect, get_object_or_404
 
 #Home
 def home(request):
@@ -8,7 +10,26 @@ def home(request):
 
 #SignUp
 def signup(request):
-  return render(request, 'signup.html')
+  if request.method == 'GET':
+    return render(request,'signup.html', {
+      'form' : UserCreationForm
+    } )   
+  else: 
+    if request.POST['password1'] == request.POST['password2']:
+      try: 
+        user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+        user.save()                
+        login(request, user)
+        return redirect('home')
+      except:
+        return render (request,'signup.html', { 
+          'form' : UserCreationForm ,
+          "error": 'Usuário já existe'                
+        } ) 
+    return render (request,'signup.html', { 
+      'form' : UserCreationForm ,
+      "error": 'senhas são diferentes'
+    } ) 
 
 #SignIn
 def signin(request):
